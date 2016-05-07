879bb820f8bd7fcf8716c3c7b62b7498f6039f40
Evan <evan@datarank.com>
2016-05-07 12:33:57 -0500

    Revert "delete hardcoded classes from model"
    This reverts commit 8a151b4afffb1237c01e0d8b9e6d9d237aa20d73.

---
+++ app/src/main/java/com/evansitzes/vocabularyflashcards/model/JapaneseKoreanWords.java
@@ -0,0 +1,25 @@
+package com.evansitzes.vocabularyflashcards.model;
+
+/**
+ * Created by evan on 9/20/15.
+ */
+public class JapaneseKoreanWords extends Flashcard {
+
+    @Override
+    public void populateInitialWordlist() {
+        foreignToEnglish.put("병원", "病院");
+        foreignToEnglish.put("책상", "つくえ");
+        foreignToEnglish.put("무슨", "何の");
+        foreignToEnglish.put("옆", "となり");
+        foreignToEnglish.put("맨션", "マンション");
+        foreignToEnglish.put("백화점", "デパート");
+        foreignToEnglish.put("교실", "教室 (きょうしつ)");
+        foreignToEnglish.put("전략", "戦略 (せんりゃく)");
+        foreignToEnglish.put("교환", "交換(こうかん)");
+        foreignToEnglish.put("물론", "もちろん");
+        foreignToEnglish.put("안돼다", "だめだ");
+        foreignToEnglish.put("A는 B와 다르다", "違う (ちがう)");
+        foreignToEnglish.put("실려합니다", "すみません");
+    }
+
+}
---
+++ app/src/main/java/com/evansitzes/vocabularyflashcards/model/NounsJapanese.java
@@ -0,0 +1,61 @@
+package com.evansitzes.vocabularyflashcards.model;
+
+/**
+ * Created by evan on 9/24/15.
+ */
+public class NounsJapanese extends Flashcard {
+    @Override
+    public void populateInitialWordlist() {
+        foreignToEnglish.put("物理", "ぶつり　physics");
+        foreignToEnglish.put("作物", "さくもつ　crops, produce");
+        foreignToEnglish.put("書物", "しょもつ　novels");
+        foreignToEnglish.put("入れ物", "いれもの　container");
+        foreignToEnglish.put("物音", "ものおと sound");
+        foreignToEnglish.put("物置", "ものおき　storage, storage room");
+        foreignToEnglish.put("元日", "がんじつ New Years Day");
+        foreignToEnglish.put("今日", "こんにち　these days, this day and era, tody");
+        foreignToEnglish.put("日中", "にっちゅう　during the day");
+        foreignToEnglish.put("日の出", "ひので　sunrise");
+        foreignToEnglish.put("日の入れ", "ひのいれ　sunset");
+        foreignToEnglish.put("来日", "らいにち　coming to Japan");
+        foreignToEnglish.put("年中", "ねんじゅう　all year around");
+        foreignToEnglish.put("年度", "ねんど　fiscal year, academic year");
+        foreignToEnglish.put("年代", "ねんだい generation (age)");
+        foreignToEnglish.put("大小", "だいしょう　large and small, all sizes");
+        foreignToEnglish.put("大気", "たいき atmosphere");
+        foreignToEnglish.put("大半", "たいはん　majority");
+        foreignToEnglish.put("中世", "ちゅうせい　medieval");
+        foreignToEnglish.put("中古", "ちゅうこ　used, second hand");
+        foreignToEnglish.put("夜中", "よなか　middle of the night, wee hours of the morning");
+        foreignToEnglish.put("世の中", "よのなか　the world (general sense. eg 彼は世の中をよく知ってる)");
+        foreignToEnglish.put("手間", "てま　work, time and effort");
+        foreignToEnglish.put("世間", "せけん　the public");
+        foreignToEnglish.put("作業", "さぎょう　work (construction or a project)");
+        foreignToEnglish.put("通行　", "つうこう　keep to x (ex 一方通行）");
+        foreignToEnglish.put("通知", "つうち　letter, notification");
+        foreignToEnglish.put("生じる", "しょうじるto come about, arise (eg. 問題)");
+        foreignToEnglish.put("用いる", "もちいる to make use of, to utilize, to employ");
+        foreignToEnglish.put("専門用語", "せんもんようご　technical words, technical jargon");
+        foreignToEnglish.put("用心", "ようじん precaution, care, caution ");
+        foreignToEnglish.put("引用", "いんよう quotation, citation");
+        foreignToEnglish.put("通用", "つうよう to be used, to gain in popularity （eg　社会で通用する)");
+        foreignToEnglish.put("日用品", "にちようひん　everyday necessities");
+        foreignToEnglish.put("地下街", "ちかがい　underground mall");
+        foreignToEnglish.put("土地", "とち　plot of land");
+        foreignToEnglish.put("地元", "じもと　local, from an area");
+        foreignToEnglish.put("本名", "ほんみょう　real name");
+        foreignToEnglish.put("発明", "はつめい　to invent");
+        foreignToEnglish.put("発生", "はっせい　to form, emerge (eg 台風が）");
+        foreignToEnglish.put("航空業界", "こうくうぎょうかい　airline industry");
+        foreignToEnglish.put("手続き", "てつづき　process, formal procedings");
+        foreignToEnglish.put("再開発", "さいかいはつ　redevelopment");
+        foreignToEnglish.put("貧しい", "まずしい　poor, needy");
+        foreignToEnglish.put("種", "たね　seed, sappling");
+        foreignToEnglish.put("収集", "しゅうしゅう　gathering up, collection");
+        foreignToEnglish.put("優れる", "すぐれる　to exceed, to surpass");
+        foreignToEnglish.put("朗らか", "ほがらか　bright, cheerfull");
+        foreignToEnglish.put("国籍", "こくせき　nationality");
+        foreignToEnglish.put("合コン", "ごうこん　party for meeting girls");
+        foreignToEnglish.put("意地になる", "いじになる　grow a backbone");
+    }
+}
