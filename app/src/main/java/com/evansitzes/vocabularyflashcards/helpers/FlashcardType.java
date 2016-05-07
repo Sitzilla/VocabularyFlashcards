8793fba6aae1bc0c3f78c0a6937c393e74ecfca2
Evan <evan@datarank.com>
2016-05-07 12:33:53 -0500

    Revert "delete old flashcard enum"
    This reverts commit 7c0d37c6f150b67cba9c1a571472ab9f262676fa.

---
+++ app/src/main/java/com/evansitzes/vocabularyflashcards/helpers/FlashcardType.java
@@ -0,0 +1,20 @@
+package com.evansitzes.vocabularyflashcards.helpers;
+
+/**
+ * Created by evan on 9/24/15.
+ */
+public class FlashcardType {
+    public final static String BASIC_KOREAN = "basicKorean";
+    public final static String JAPANESE_KOREAN = "japaneseKorean";
+    public final static String INTERMEDIATE_KOREAN = "intermediateKorean";
+    public final static String ADVANCED_KOREAN = "advancedKorean";
+    public final static String READING_JAPANESE = "readingJapanese";
+    public final static String NOUNS_JAPANESE = "nounsJapanese";
+    public final static String KANJI_JAPANESE = "kanjiJapanese";
+    public final static String ADJECTIVES_JAPANESE = "adjectivesJapanese";
+    public final static String SHINBUN_JAPANESE = "shinbunJapanese";
+    public final static String BASIC_CHINESE = "basicChinese";
+    public final static String INTERMEDIATE_CHINESE = "intermediateChinese";
+    public final static String ADVANCED_CHINESE = "advancedChinese";
+
+}
